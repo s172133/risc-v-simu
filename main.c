76eef65e06150efa2941653410742bc4e34be445
Mikkel Wøidemann Klæbel Blomsterberg <s172133@dtu.dk>
2021-11-15 14:22:45 +0100

    switch case created

--- main.c
+++ main.c
@@ -7,23 +7,33 @@ int main(){
     printf("********** Starting **********\n");
 
     uint32_t pc = 0; // Program counter
-    uint32_t reg[3]; // Register
+    uint32_t reg[32]; // Register
 
-    malloc(32);
+    malloc( 42 * sizeof(uint32_t) ); // Allocate binary code
 
-
+    // OPEN FILE
     FILE *fp = fopen("binary.txt", "r");
-
     if(fp == NULL){
         perror("Unable to open file!");
         exit(1);
     }
 
+
+    int opcode;
     char str[40];
- 
-     while(fgets(str, sizeof(str), fp) != NULL) {
-         fputs(str, stdout);
-         
+     while(pc != 10) {
+         pc += 1;
+
+
+         switch (opcode) {
+
+             case 0x13:
+                 reg[rd] = reg[rs1] + imm;
+                 break;
+             default:
+                 cout << "Opcode " << opcode << " not yet implemented" << endl;
+                 break;
+         }
      }
     fclose(fp);
     return 0;
