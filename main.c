c44e6f39beeb876e3554267a7f91e9527645f188
s172133 <79192521+s172133@users.noreply.github.com>
2021-11-15 13:24:22 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    printf("********** Starting **********\n");
+
+    FILE *fp = fopen("binary.txt", "r");
+
+    if(fp == NULL){
+        perror("Unable to open file!");
+        exit(1);
+    }
+
+    char str[40];
+ 
+     while(fgets(str, sizeof(str), fp) != NULL) {
+         fputs(str, stdout);
+         
+     }
+    fclose(fp);
+    return 0;
+}
